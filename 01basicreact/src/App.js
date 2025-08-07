d09ea42de89c4b05146c8bc5c5215c5b2be6fc98
Utkarsh <utkarsh1480759@gmail.com>
2025-08-07 22:55:10 +0530

    Hello

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div><h1> Hello world | Utkarsh | I learn how to install React vite and react 
+      Installation of React = create-react-app name and from react  npm command trough and npm i in React vite initally node module is not present we need to install node module using command npm i for understand react go to json file and see that.
+      </h1></div>
+    
+  );
+}
+
+export default App;
